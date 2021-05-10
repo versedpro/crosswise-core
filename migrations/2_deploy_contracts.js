5283b9615a05de557e20a5e695e0557f568120e3
cryptochaindev <ultraexpert33@gmail.com>
2021-05-10 16:53:18 +0800

    Deleted migration folder

--- migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-const FoxchainFactory = artifacts.require("CrosswiseFactory");
-// require('dotenv').config();
-
-module.exports = function (deployer) {
-  deployer.deploy(FoxchainFactory, '0xa3C4fFD5Fb5f9F4c1DA865EF2bD2B6EC65B6c235');
-};
